90b5c4aecb5dd718e4abdfef56feafaab35a395a
Finlay Muir <116896403+finmuir@users.noreply.github.com>
2023-04-17 12:32:38 +0100

    Update rules.cpp

--- Forms/rules/rules.cpp
+++ Forms/rules/rules.cpp
@@ -1,1 +1,27 @@
+#include "rules.h"
+#include "ui_rules.h"
+
+rules::rules(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::rules)
+{
+    ui->setupUi(this);
+}
+
+
+void rules::on_pushButton_clicked()
+{
+close();
+}
+
+
+
+
+
+
+rules::~rules()
+{
+    delete ui;
+}
+
 
