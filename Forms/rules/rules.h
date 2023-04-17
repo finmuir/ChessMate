b8d1935825364906d76a4c9ded3b1eb0e63587dd
Finlay Muir <116896403+finmuir@users.noreply.github.com>
2023-04-17 12:33:34 +0100

    Update rules.h

--- Forms/rules/rules.h
+++ Forms/rules/rules.h
@@ -1,1 +1,25 @@
+#ifndef RULES_H
+#define RULES_H
 
+#include <QWidget>
+
+namespace Ui {
+class rules;
+}
+
+class rules : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit rules(QWidget *parent = nullptr);
+    ~rules();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::rules *ui;
+};
+
+#endif // RULES_H
